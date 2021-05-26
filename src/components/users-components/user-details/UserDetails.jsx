db86205d0a713d00e87b5abc20b225dc6ca5cf54
pnuzunov <pnuzunov@gmail.com>
2021-05-26 18:24:03 +0300

    Update 26-05-2021

--- src/components/users-components/user-details/UserDetails.jsx
+++ src/components/users-components/user-details/UserDetails.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
 import { getLoggedUser } from "../../../services/AuthService";
 import { deleteUser, getUser } from "../../../services/UserService";
-
 
 export function UserDetails(props) {
 
@@ -10,6 +10,11 @@ export function UserDetails(props) {
     const [redirect, setRedirect] = useState(false);
 
     const loggedUser = getLoggedUser();
+
+    const linkStyle = {
+        textDecoration: 'none',
+        color: 'white'
+    }
 
     useEffect(_ => {
         if(props.computedMatch.params.user) {
@@ -34,7 +39,8 @@ export function UserDetails(props) {
                 <span>Name: </span>
                 <span>{user && user.name}</span>
             </div>
-            {loggedUser && loggedUser.admin && <div> <button className="btn btn-danger d-block m-auto" onClick={onUserDelete}>Disable account</button></div>}
+            {loggedUser && loggedUser.admin && user && <button className="btn btn-success"><Link style={linkStyle} to={`/users/edit/${user.id}`}>Edit</Link></button>}
+            {loggedUser && loggedUser.admin && <div> <button className="btn btn-danger" onClick={onUserDelete}>Disable account</button></div>}
         </div>
         </>
     )
--- src/components/users-components/user-entry/UserEntry.jsx
+++ src/components/users-components/user-entry/UserEntry.jsx
@@ -5,8 +5,7 @@ export function UserEntry( {user} ) {
     return (
         <>
         <div className="d-flex">
-        <Link to={`/users/${user.id}`}>{user.name}</Link>
-        <Link to={`/users/edit/${user.id}`}> | Edit</Link>
+        <Link to={`/users/${user.id}`}>{user.name} (ID {user.id})</Link>
         </div>
         </>
     )
